8f72cb4fad50a00c1821a533e5dd167f80e208ce
Adem Halilović <adem.halilovic@edu.fit.ba>
2022-03-22 14:28:14 +0100

    Employee-model

---
+++ Certigon-Task-API/Certigon-Task-API/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Certigon_Task_API.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
